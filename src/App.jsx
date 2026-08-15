4dbe08f6c8e5b7ad91f39167ffee300d9a789bc5
Abdullah-18155 <hello.codedbyabdullah@gmail.com>
2026-08-15 14:10:55 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <main className="relative z-0">
+
+        <div className="bg-glow" aria-hidden={true}></div>
+
+        <h1>Hello World</h1>
+
+      </main>
+    </>
+  )
+}
+
+export default App
